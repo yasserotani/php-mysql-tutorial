f8d7f982747ba65a0607ae8ccfc59999346f6405
yasserotani <yasserotane@gmail.com>
2025-09-17 22:15:04 +0300

    test

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php
 // USING MYSQLI METHOD
 
-// connect to the database
+// (1) connect to the database
 $conn = mysqli_connect('localhost', 'shaun', 'test1234', 'ninja_pizza', 3307);
 
 // check connection
 if (!$conn) {
 	// echo 'Connection error: ' . mysqli_connect_error();
 }
+//  (2) write query for all pizzas
+$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY create_at';
 
-// write query for all pizzas
-$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
-
-// get the result set (set of rows)
+//  (3) get the result set (set of rows)
 $result = mysqli_query($conn, $sql);
-
-// fetch the resulting rows as an arra
+print_r($result);
+//  (4) fetch the resulting rows as an arra 
+// MYSQLI_ASSOC mode that mean we want the result as a associated array
 $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-// free the $result from memory (good practise)
+// (6 Good pracitce) free the $result from memory (good practise)
 mysqli_free_result($result);
 
-// close connection
+// (7 Good pracitce) close connection
 mysqli_close($conn);
 
 print_r($pizzas);
