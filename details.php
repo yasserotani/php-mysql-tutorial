2f1986860c2efc58810a5a435f95e64accede0bf
yasserotani <yasserotane@gmail.com>
2025-09-22 11:38:42 +0300

    test

--- details.php
+++ details.php
@@ -1,67 +1,67 @@
-<?php 
+<?php
 
-	include('config/db_connect.php');
+include('config/db_connect.php');
 
-	if(isset($_POST['delete'])){
+if (isset($_POST['delete'])) {
 
-		$id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+	$id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
 
-		$sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+	$sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
 
-		if(mysqli_query($conn, $sql)){
-			header('Location: index.php');
-		} else {
-			echo 'query error: '. mysqli_error($conn);
-		}
-
+	if (mysqli_query($conn, $sql)) {
+		header('Location: index.php');
+	} else {
+		echo 'query error: ' . mysqli_error($conn);
 	}
 
-	// check GET request id param
-	if(isset($_GET['id'])){
-		
-		// escape sql chars
-		$id = mysqli_real_escape_string($conn, $_GET['id']);
+}
 
-		// make sql
-		$sql = "SELECT * FROM pizzas WHERE id = $id";
+// check GET request id param
+if (isset($_GET['id'])) {
 
-		// get the query result
-		$result = mysqli_query($conn, $sql);
+	// escape sql chars
+	$id = mysqli_real_escape_string($conn, $_GET['id']);
 
-		// fetch result in array format
-		$pizza = mysqli_fetch_assoc($result);
+	// make sql
+	$sql = "SELECT * FROM pizzas WHERE id = $id";
 
-		mysqli_free_result($result);
-		mysqli_close($conn);
+	// get the query result
+	$result = mysqli_query($conn, $sql);
 
-	}
+	// fetch result in array format
+	$pizza = mysqli_fetch_assoc($result);
+
+	mysqli_free_result($result);
+	mysqli_close($conn);
+
+}
 
 ?>
 
 <!DOCTYPE html>
 <html>
 
-	<?php include('templates/header.php'); ?>
+<?php include('templates/header.php'); ?>
 
-	<div class="container center grey-text">
-		<?php if($pizza): ?>
-			<h4><?php echo $pizza['title']; ?></h4>
-			<p>Created by <?php echo $pizza['email']; ?></p>
-			<p><?php echo date($pizza['created_at']); ?></p>
-			<h5>Ingredients:</h5>
-			<p><?php echo $pizza['ingredients']; ?></p>
+<div class="container center grey-text">
+	<?php if ($pizza): ?>
+		<h4><?php echo $pizza['title']; ?></h4>
+		<p>Created by <?php echo $pizza['email']; ?></p>
+		<p><?php echo date($pizza['create_at']); ?></p>
+		<h5>Ingredients:</h5>
+		<p><?php echo $pizza['ingredients']; ?></p>
 
-			<!-- DELETE FORM -->
-			<form action="details.php" method="POST">
-				<input type="hidden" name="id_to_delete" value="<?php echo $pizza['id']; ?>">
-				<input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
-			</form>
+		<!-- DELETE FORM -->
+		<form action="details.php" method="POST">
+			<input type="hidden" name="id_to_delete" value="<?php echo $pizza['id']; ?>">
+			<input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+		</form>
 
-		<?php else: ?>
-			<h5>No such pizza exists.</h5>
-		<?php endif ?>
-	</div>
+	<?php else: ?>
+		<h5>No such pizza exists.</h5>
+	<?php endif ?>
+</div>
 
-	<?php include('templates/footer.php'); ?>
+<?php include('templates/footer.php'); ?>
 
 </html>
