b8666d8637e320de20f1454be3a67e7d331e8582
yasserotani <yasserotane@gmail.com>
2025-09-28 13:12:38 +0300

    test

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
-	// connect to the database
-	$conn = mysqli_connect('localhost', 'shaun', 'test1234', 'ninja_pizza');
+// connect to the database
+$conn = mysqli_connect('localhost', 'shaun', 'test1234', 'ninja_pizza', 3307);
 
-	// check connection
-	if(!$conn){
-		echo 'Connection error: '. mysqli_connect_error();
-	}
+// check connection
+if (!$conn) {
+	echo 'Connection error: ' . mysqli_connect_error();
+}
 
 ?>
--- index.php
+++ index.php
@@ -1,59 +1,59 @@
 <?php
 
-	include('config/db_connect.php');
+include('config/db_connect.php');
 
-	// write query for all pizzas
-	$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
+// write query for all pizzas
+$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY create_at';
 
-	// get the result set (set of rows)
-	$result = mysqli_query($conn, $sql);
+// get the result set (set of rows)
+$result = mysqli_query($conn, $sql);
 
-	// fetch the resulting rows as an array
-	$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+// fetch the resulting rows as an array
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-	// free the $result from memory (good practise)
-	mysqli_free_result($result);
+// free the $result from memory (good practise)
+mysqli_free_result($result);
 
-	// close connection
-	mysqli_close($conn);
+// close connection
+mysqli_close($conn);
 
 
 ?>
 
 <!DOCTYPE html>
 <html>
-	
-	<?php include('templates/header.php'); ?>
 
-	<h4 class="center grey-text">Pizzas!</h4>
+<?php include('templates/header.php'); ?>
 
-	<div class="container">
-		<div class="row">
+<h4 class="center grey-text">Pizzas!</h4>
 
-			<?php foreach($pizzas as $pizza): ?>
+<div class="container">
+	<div class="row">
 
-				<div class="col s6 m4">
-					<div class="card z-depth-0">
-						<img src="img/pizza.svg"class="pizza">
-						<div class="card-content center">
-							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
-							<ul class="grey-text">
-								<?php foreach(explode(',', $pizza['ingredients']) as $ing): ?>
-									<li><?php echo htmlspecialchars($ing); ?></li>
-								<?php endforeach; ?>
-							</ul>
-						</div>
-						<div class="card-action right-align">
-							<a class="brand-text" href="details.php?id=<?php echo $pizza['id'] ?>">more info</a>
-						</div>
+		<?php foreach ($pizzas as $pizza): ?>
+
+			<div class="col s6 m4">
+				<div class="card z-depth-0">
+					<img src="img/pizza.svg" class="pizza">
+					<div class="card-content center">
+						<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+						<ul class="grey-text">
+							<?php foreach (explode(',', $pizza['ingredients']) as $ing): ?>
+								<li><?php echo htmlspecialchars($ing); ?></li>
+							<?php endforeach; ?>
+						</ul>
+					</div>
+					<div class="card-action right-align">
+						<a class="brand-text" href="details.php?id=<?php echo $pizza['id'] ?>">more info</a>
 					</div>
 				</div>
+			</div>
 
-			<?php endforeach; ?>
+		<?php endforeach; ?>
 
-		</div>
 	</div>
+</div>
 
-	<?php include('templates/footer.php'); ?>
+<?php include('templates/footer.php'); ?>
 
 </html>
